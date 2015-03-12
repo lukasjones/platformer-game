c03ebed9f0a418519f40b976b4f14968af71ebd7
Lukas Jones <joneslukas21@gmail.com>
2015-03-12 16:40:36 -0700

    changed app name

--- app/controllers/Oauth_controller.rb
+++ app/controllers/Oauth_controller.rb
@@ -9,7 +9,7 @@ before do
   @client_id = ENV['FB_CLIENT_ID']
   @client_secret = ENV['FB_CLIENT_SECRET']
   # @host = ENV['HOST']
-  @host = 'https://radiant-escarpment-6191.herokuapp.com/'
+  @host = 'https://lukasplatforms.herokuapp.com/'
   session[:oauth] ||= {}
 end
 
